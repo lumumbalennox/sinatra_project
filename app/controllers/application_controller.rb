ee54eef4828eeaf51d83946cbeb3421175257a28
Lennox Otieno Lumumba <lennox.lumumba@student.moringaschool.com>
2023-04-01 16:51:41 +0300

    Deletions routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,25 @@ class ApplicationController < Sinatra::Base
     students.to_json
   end
 
+  # Deleting a student
+  delete './students/:id' do
+    student=Student.find(params[:id])
+    student.destroy
+    student.to_json
+  end
+
 
   get "/parents" do
     # get all the parentsts from the db
     parents = Parent.all
     parents.to_json
+  end
+
+  # Deleting a parent
+  delete './parents/:id' do
+    parent=Parent.find(params[:id])
+    parent.destroy
+    parent.to_json
   end
 
   get "/schools" do
@@ -21,16 +35,38 @@ class ApplicationController < Sinatra::Base
     schools.to_json
   end
 
+  # Deleting a school
+  delete './schools/:id' do
+    school=School.find(params[:id])
+    school.destroy
+    school.to_json
+  end
+
+
   get "/siblings" do
     # get all the parentsts from the db
     siblings = Sibling.all
     siblings.to_json
   end
 
+  # Deleting sibling
+  delete './siblings/:id' do
+    sibling=Sibling.find(params[:id])
+    sibling.destroy
+    sibling.to_json
+  end
+
   get "/constraints" do
-    # get all the parentsts from the db
+    # get all the constraints from the db
     constraints = Constraint.all
     constraints.to_json
+  end
+
+  # Deleting a constarint
+  delete './constraints/:id' do
+    constraint=Constraint.find(params[:id])
+    constraint.destroy
+    constraint.to_json
   end
 
 end
