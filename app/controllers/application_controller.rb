92b1e4c5296bf87608a0bcf9570d1b5c0b5593dc
Lennox Otieno Lumumba <lennox.lumumba@student.moringaschool.com>
2023-04-01 16:38:36 +0300

    Additions Made

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,35 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/students" do
+    # get all the students from the db
+    students = Student.all
+    students.to_json
+  end
+
+
+  get "/parents" do
+    # get all the parentsts from the db
+    parents = Parent.all
+    parents.to_json
+  end
+
+  get "/schools" do
+    # get all the parentsts from the db
+    schools = School.all
+    schools.to_json
+  end
+
+  get "/siblings" do
+    # get all the parentsts from the db
+    siblings = Sibling.all
+    siblings.to_json
+  end
+
+  get "/constraints" do
+    # get all the parentsts from the db
+    constraints = Constraint.all
+    constraints.to_json
   end
 
 end
